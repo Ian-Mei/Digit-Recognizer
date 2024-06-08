7ffb5768574e21d4000b6b235ccc8133700cca5f
Ian Mei <ianmmei246@gmail.com>
2024-06-08 12:25:04 -0400

    Version 1 complete

--- Website/app.py
+++ Website/app.py
@@ -1,13 +1,12 @@
-from flask import Flask, render_template, request, jsonify
-import pickle
+from flask import Flask, request, jsonify, render_template
 import numpy as np
-import pandas as pd
+import pickle
 
 app = Flask(__name__)
 
 # Load the model
-with open('digit_classification.pkl', 'rb') as file:
-    model = pickle.load(file)
+with open('digit_classification.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
 
 @app.route('/')
 def home():
@@ -15,21 +14,24 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get the data from the POST request
-    data = request.get_json(force=True)
-    # poop = pd.DataFrame(data['resizedArray'])
-    # poop = poop.drop('Unnamed: 0', axis=1)
-    # np_data = data.values
-    # reshaped_data = np_data.reshape(-1,28, 28,1)
-    img = np.array(data['resizedArray']).reshape(-1,28,28,1)
-    # img.drop('Unnamed: 0', axis=1)
-    
+    try:
+        data = request.get_json(force=True)
+        resized_array = data['resizedArray']
+        
+        # Convert the list to a numpy array
+        image_array = np.array(resized_array, dtype=np.float32)
+        
+        # Reshape the array to match the input shape expected by the model
+        image_array = image_array.reshape(1, 28, 28, 1)
 
-    # Make a prediction using the model
-    prediction = model.predict(img)
-    prediction = np.argmax(prediction,axis = 1)
-    # Return the prediction
-    return jsonify(prediction.tolist())
+        # Make a prediction
+        predictions = model.predict(image_array)
+        predicted_digit = np.argmax(predictions, axis=1)[0]
+        prediction_confidences = predictions[0].tolist()
+
+        return jsonify(predicted_digit=int(predicted_digit), prediction_confidences=prediction_confidences)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(debug=True)
